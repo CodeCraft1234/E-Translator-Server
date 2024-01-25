5d155d758e520f89377df82c1bf3f4de701389d8
robiul704 <robiulislam29935@gmail.com>
2024-01-25 21:58:18 +0600

    deployment issue solved

--- Index.js
+++ Index.js
@@ -10,8 +10,8 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Translator:codecraft704@robiul.13vbdvd.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion } = require('mongodb'); 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@robiul.13vbdvd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {

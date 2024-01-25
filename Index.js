b7f3f19d1cd1c891144234cbbf2eaa5bd9f02e51
CodeCraft1234 <codecraft271@gmail.com>
2024-01-26 00:32:47 +0530

    fix bugs

--- Index.js
+++ Index.js
@@ -1,17 +1,17 @@
-const express=require('express')
-const cors=require('cors')
-require('dotenv').config()
-const app=express()
-const port=process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+
 
 //MIADLEWERE
 
 app.use(cors())
 app.use(express.json())
 
+const port =  process.env.PORT || 6000;
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb'); 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@robiul.13vbdvd.mongodb.net/?retryWrites=true&w=majority`;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@robiul.13vbdvd.mongodb.net/?retryWrites=true&w=majority`;

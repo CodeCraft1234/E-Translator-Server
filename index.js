b7a7ad72abdaf752d0cff8c823f1d62e11bb5348
Banjirsultana90 <banjirsultana15@gmail.com>
2024-02-28 11:17:24 +0600

    verify token created

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const { Server } = require("socket.io");
 const cookieParser = require("cookie-parser");
 const SSLCommerzPayment = require("sslcommerz-lts");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-
 const socketIo = require("socket.io");
 
 const server = http.createServer(app);
@@ -23,7 +22,8 @@ const io = new Server(server, {
 
 app.use(
   cors({
-    origin: ["https://etranslator.netlify.app"],
+    // origin: ["https://etranslator.netlify.app"],
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
@@ -31,7 +31,8 @@ app.use(
 app.use(cors(
   {
     origin: [
-      'https://etranslator.netlify.app'
+      // 'https://etranslator.netlify.app'
+      "http://localhost:5173",
 
 
     ],
@@ -64,7 +65,7 @@ const is_live = false; //true for live, false for sandbox
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
     // Send a ping to confirm a successful connection
 
     const usersInfocollection = client.db("E-Translator").collection("usersInfo");
@@ -83,23 +84,6 @@ async function run() {
 
 
 
-    // suggestions api
-
-    app.get('/api/suggestions', async (req, res) => {
-      try {
-
-        const data = await translationsuggestion.findOne({});
-        const suggestions = data.translation_suggestions;
-      
-        const formattedSuggestions = suggestions.map(({ letter, words }) => ({ letter, words }));
-
-        console.log(formattedSuggestions);
-        res.json(formattedSuggestions);
-      } catch (error) {
-        console.error('Error fetching translation suggestions:', error);
-        res.status(500).json({ error: 'Internal server error' });
-      }
-    });
 
 
     // auth api
@@ -119,6 +103,56 @@ async function run() {
       res.send({ success: true });
       // res.send(user)
     });
+
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+
+
+
+    // use verify admin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersInfocollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
+
+    // Admin route
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      const query = { email: email };
+      const user = await usersInfocollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
+
     app.post("/logout", async (req, res) => {
       const user = req.body;
       console.log("loging out", user);
@@ -148,13 +182,31 @@ async function run() {
     });
 
 
+    // suggestions api
+
+    app.get('/api/suggestions', async (req, res) => {
+      try {
+
+        const data = await translationsuggestion.findOne({});
+        const suggestions = data.translation_suggestions;
+
+        const formattedSuggestions = suggestions.map(({ letter, words }) => ({ letter, words }));
+
+        // console.log(formattedSuggestions);
+        res.json(formattedSuggestions);
+      } catch (error) {
+        console.error('Error fetching translation suggestions:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
+
     //------------------------------------------------------------------------
     //                        translation history part
     //------------------------------------------------------------------------
 
     app.post("/api/history", async (req, res) => {
       try {
-        await client.connect();
+        // await client.connect();
 
         const translation = req.body;
 
@@ -165,13 +217,15 @@ async function run() {
         console.error(error);
         res.status(500).json({ error: "Internal Server Error" });
       } finally {
-        await client.close();
+        // await client.close();
       }
     });
 
+
+
     app.get("/api/history", async (req, res) => {
       try {
-        await client.connect();
+        // await client.connect();
 
         const translations = await translationCollection
           .find()
@@ -183,7 +237,7 @@ async function run() {
         console.error(error);
         res.status(500).json({ error: "Internal Server Error" });
       } finally {
-        await client.close();
+        // await client.close();
       }
     });
 
@@ -320,12 +374,12 @@ async function run() {
       const result = await commentsInfocollection.find().toArray();
       res.send(result);
     });
-  
-    app.get('/blogComment/get/:id',async(req,res)=>{
-      const id=req.params.id
-      const filter={id:id}
-        const result=await commentsInfocollection.find(filter).toArray()
-        res.send(result)
+
+    app.get('/blogComment/get/:id', async (req, res) => {
+      const id = req.params.id
+      const filter = { id: id }
+      const result = await commentsInfocollection.find(filter).toArray()
+      res.send(result)
     })
 
     //--------------------------------------------
@@ -345,8 +399,8 @@ async function run() {
         total_amount: order.price,
         currency: "BDT",
         tran_id: tran_id, // use unique tran_id for each api call
-        success_url: `https://e-translator-server.vercel.app/payment/success/${tran_id}`,
-        fail_url: `https://e-translator-server.vercel.app/payment/fail/${tran_id}`,
+        success_url: `http://localhost:5000/payment/success/${tran_id}`,
+        fail_url: `http://localhost:5000/payment/fail/${tran_id}`,
         cancel_url: "http://localhost:3030/cancel",
         ipn_url: "http://localhost:3030/ipn",
         shipping_method: "Courier",
@@ -444,6 +498,20 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // const express = require("express");
 // const cors = require("cors");
